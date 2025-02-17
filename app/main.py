f9d62b47b369f565c872f8b09601fba6b4bdd18c
andrewvm17 <andrewtce10@gmail.com>
2025-02-17 15:18:48 -0500

    enable cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import vanishing_point
 from app.core import logging_config  
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # List of allowed origins (frontend URLs)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(vanishing_point.router)
 
